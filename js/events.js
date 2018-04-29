7b9d371ac41535884682cb02c20e56cbd189e36f
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-29 22:55:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $(this).addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(e) {
+    const key = parseInt(e.which || e.detail);
+    if ( key === 71) {
+      alert("You pressed G.");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
