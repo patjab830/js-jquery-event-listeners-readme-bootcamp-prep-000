a496d5a14fc3f88510e7953f7ca3f5852bf97ad0
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-29 22:54:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,8 +14,16 @@ function frameIt() {
 function pressIt() {
   $("input").on("keydown", function() {
     const key = parseInt(e.which || e.detail);
-    if ( e.which === )
-  })
+    if ( key === 71) {
+      alert("You pressed G.");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
 }
 
 $(document).ready(function(){
